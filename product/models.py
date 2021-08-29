ef16983a5035509c986e783135067e8d5bff9f58
Swannhs <princeapurbask@gmail.com>
2021-08-29 18:25:56 +0600

    Configured rest api for products

--- product/models.py
+++ product/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+    id = models.AutoField(primary_key=True, editable=False)
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    createdAt = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.name)
